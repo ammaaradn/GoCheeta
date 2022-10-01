a7d18e3eb0d7c0d8ede5a3a6f84c27bf8f6323cb
ammaaradn <ammaaradn2@gmail.com>
2022-10-01 11:28:51 +0530

    add client

---
+++ clientcheeta/src/main/webapp/app.js
@@ -0,0 +1,15 @@
+class FormValidation{
+ 
+    showErrorMsg(index,msg){
+        const form_group = document.getElementsByClassName('form-group')[index]
+        form_group.classList.add('error')
+        form_group.getElementsByTagName('span')[0].textContent = msg   
+    }
+    showSuccessMsg(index){
+        const form_group = document.getElementsByClassName('form-group')[index]
+        form_group.classList.remove('error')
+        form_group.classList.add('success')
+    }
+   
+
+}
